c2be4955292d7b2e7888ad96cb5b15bfd39f83b4
Omar Hany <89995953+0marHany@users.noreply.github.com>
2024-06-17 01:58:54 +0300

    Update Collage.routes.js

--- src/Collage/routes/Collage.routes.js
+++ src/Collage/routes/Collage.routes.js
@@ -5,7 +5,7 @@ const { getAllCollage, addCollage, putCollage, deleteCollage, getCollage } = req
 const routes =require("express").Router();
 
 routes.get("/collage/:uni_id",getAllCollage);
-routes.get("/collage/:col_id",getCollage);
+routes.get("/collage/specific/:col_id",getCollage);
 routes.post("/collage",Upload.single('imageUrl'),addCollage);
 routes.put("/collage/:id",Upload.single('imageUrl'),putCollage);
 routes.delete("/collage/:id",deleteCollage);
