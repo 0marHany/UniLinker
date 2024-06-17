e32f0d2231117a4ef226c95c6d3df37eea55ef5a
Omar Hany <89995953+0marHany@users.noreply.github.com>
2024-06-17 14:30:14 +0300

    addImage

--- src/Scholarship/routes/Scholarship.routes.js
+++ src/Scholarship/routes/Scholarship.routes.js
@@ -1,9 +1,11 @@
 const { getAllScholar, addScholar, putScholar, deleteScholar } = require("../controller/Scholarship.controller");
 
+const { Upload } = require("../../../common/services/uploadFile");
+
 const routes =require("express").Router();
 
 routes.get("/scholar",getAllScholar);
-routes.post("/scholar",addScholar);
+routes.post("/scholar",Upload.single('imageUrl'),addScholar);
 routes.put("/scholar/:id",putScholar);
 routes.delete("/scholar/:id",deleteScholar);
 
