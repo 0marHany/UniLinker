2717499467a0a3883df5fb1a259bb366aeaa1e1c
Omar Hany <89995953+0marHany@users.noreply.github.com>
2024-06-17 14:24:48 +0300

    provide Image

--- src/Events/routes/event.routes.js
+++ src/Events/routes/event.routes.js
@@ -1,9 +1,11 @@
 const { getAllEvent, addEvent, putEvent, deleteEvent } = require("../controller/event.collage");
 
+const { Upload } = require("../../../common/services/uploadFile");
+
 const routes =require("express").Router();
 
 routes.get("/event",getAllEvent);
-routes.post("/event",addEvent);
+routes.post("/event",Upload.single('imageUrl'),addEvent);
 routes.put("/event/:id",putEvent);
 routes.delete("/event/:id",deleteEvent);
 
